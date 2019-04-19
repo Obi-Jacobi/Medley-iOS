cd658449e97a3a4ba212eabd110c14bae11b3378
Jacob Wilson <jacobtrwilson@icloud.com>
2019-04-19 12:28:47 -0500

    Initial Commit

---
+++ Medley-iOSTests/Medley_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  Medley_iOSTests.swift
+//  Medley-iOSTests
+//
+//  Created by Jacob Wilson on 4/19/19.
+//  Copyright © 2019 Jacob Wilson. All rights reserved.
+//
+
+import XCTest
+@testable import Medley_iOS
+
+class Medley_iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
